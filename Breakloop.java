ad6aae9205cbdc08350988ba1320307952440cc1
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-07 12:46:37 +0530

    Add files via upload

---
+++ Breakloop.java
@@ -0,0 +1,12 @@
+class Breakloop{
+    public static void main(String args[])
+    {
+        System.out.println("break example");
+        for(int i=0;i<5;i++)
+        {
+            if(i==2)
+            break;
+        System.out.println(i);
+        }
+        }
+}
