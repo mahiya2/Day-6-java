58c6fb7b0fda4538aff4e82554dee9b8478c202b
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-07 13:15:58 +0530

    Add files via upload

---
+++ Continueloop.java
@@ -0,0 +1,12 @@
+class Continueloop
+{
+    public static void main(String args[]){
+        System.out.println("continue example");
+        for(int i=0;i<5;i++)
+        {
+            if(i==2)
+             continue;
+        System.out.println(i);
+        }
+    }
+}
